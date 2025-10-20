8fda9991321adbb9424de511d772504e171d086c
Дмитрий Корольков <Tyuiu.KorolkovDS@outlook.com>
2025-10-20 19:11:34 +0500

    Add project files.

---
+++ Tyuiu.KorolkovDS.Sprint3.Task0.V9.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Tyuiu.KorolkovDS.Sprint3.Task0.V9.Lib;
+
+namespace Tyuiu.KorolkovDS.Sprint3.Task0.V9.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            double value = 0.5;
+            int startValue = 1;
+            int stopValue = 15;
+            double res = ds.GetSumSeries(value, startValue, stopValue);
+            double wait =0.43;
+            Assert.AreEqual(res, wait);
+        }
+    }
+}
---
+++ Tyuiu.KorolkovDS.Sprint3.Task0.V9/Program.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.KorolkovDS.Sprint3.Task0.V9.Lib;
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+Console.WriteLine("***************************************************************************");
+DataService ds = new DataService();
+double value = 0.5;
+int startValue = 1;
+int stopValue = 15;
+Console.WriteLine("Переменная Х: " +  value);
+Console.WriteLine("Старт шага = " +  startValue);
+Console.WriteLine("Конец шага = " +  stopValue);
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ                                                               *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("Сумма ряда = " + ds.GetSumSeries(value, startValue, stopValue)); 
+Console.ReadKey();
