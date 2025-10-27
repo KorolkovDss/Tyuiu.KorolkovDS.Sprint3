373e326ae3506b8ee6ec176b320c3d5d737bc00f
Дмитрий Корольков <Tyuiu.KorolkovDS@outlook.com>
2025-10-27 21:58:13 +0500

    Task6

---
+++ Tyuiu.KorolkovDS.Sprint3.Task6.V28.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.KorolkovDS.Sprint3.Task6.V28.Lib;
+
+namespace Tyuiu.KorolkovDS.Sprint3.Task6.V28.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int startValue = 13;
+            int stopValue = 19;
+            int res = ds.GetSumTheDivisors(startValue, stopValue);
+            int wait = 16;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.KorolkovDS.Sprint3.Task6.V28/Program.cs
@@ -0,0 +1,15 @@
+﻿using Tyuiu.KorolkovDS.Sprint3.Task6.V28.Lib;
+
+DataService ds  = new DataService();
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+Console.WriteLine("***************************************************************************");
+int startValue = 13;
+int stopValue = 19;
+Console.WriteLine("Начало отрезка = " + startValue);
+Console.WriteLine("Конец отрезка = " + stopValue);
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ                                                               *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("Количестов делителей, которые меньше 7 = " + ds.GetSumTheDivisors(startValue, stopValue));
+Console.ReadKey();
