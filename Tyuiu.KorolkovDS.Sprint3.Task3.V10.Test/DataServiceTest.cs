50d35c7aa52de0df127ac8c3eb85c8d9beac447c
Дмитрий Корольков <Tyuiu.KorolkovDS@outlook.com>
2025-10-27 19:27:50 +0500

    Task3

---
+++ Tyuiu.KorolkovDS.Sprint3.Task3.V10.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Tyuiu.KorolkovDS.Sprint3.Task3.V10.Lib;
+
+namespace Tyuiu.KorolkovDS.Sprint3.Task3.V10.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            string value = "gdfppf vfppt p";
+            char item = 'c';
+            string res = ds.DeleteCharInString(value, item);
+            string wait = "gdff vft ";
+            Assert.AreEqual(wait, res);
+
+        }
+    }
+}
---
+++ Tyuiu.KorolkovDS.Sprint3.Task3.V10/Program.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.KorolkovDS.Sprint3.Task3.V10.Lib;
+DataService ds = new DataService();
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+Console.WriteLine("***************************************************************************");
+string value = "gdfppf vfppt p";
+char chr = 'p';
+Console.WriteLine("Исходная строка: " + value);
+Console.WriteLine("Символ который нужно удалить: " + chr);
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ                                                               *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("Строка без символа: " + ds.DeleteCharInString(value, chr));
+Console.ReadKey();
+
