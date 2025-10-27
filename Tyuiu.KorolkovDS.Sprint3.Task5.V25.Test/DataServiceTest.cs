52907793e26316eea64cfd2d3880cf99779ae7e7
Дмитрий Корольков <Tyuiu.KorolkovDS@outlook.com>
2025-10-27 20:50:29 +0500

    Task5

---
+++ Tyuiu.KorolkovDS.Sprint3.Task5.V25.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using Tyuiu.KorolkovDS.Sprint3.Task5.V25.Lib;
+
+namespace Tyuiu.KorolkovDS.Sprint3.Task5.V25.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int x = 2;
+            int startValue1 = 1;
+            int startValue2 = 1;
+            int stopValue1 = 3;
+            int stopValue2 = 10;
+            double res = ds.GetSumSumSeries(x,startValue1,startValue2,stopValue1 ,stopValue2);
+            double wait = 6133.748;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.KorolkovDS.Sprint3.Task5.V25/Program.cs
@@ -0,0 +1,24 @@
+﻿using Tyuiu.KorolkovDS.Sprint3.Task5.V25.Lib;
+
+DataService ds = new DataService();
+int x = 2;
+int startValue1 = 1;
+int startValue2 = 1;
+int stopValue1 = 3;
+int stopValue2 = 10;
+double sumSeries = 0;
+int i, k;
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("Старт шага первой суммы ряда = " + startValue1);
+Console.WriteLine("Конец шага первой суммы ряда = " + stopValue1);
+Console.WriteLine("Старт шага второй суммы ряда = " + startValue2);
+Console.WriteLine("Конец шага второй суммы ряда = " + stopValue2);
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ                                                               *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("Сумма ряда = " + ds.GetSumSumSeries(x, startValue1, startValue2, stopValue1, stopValue2));
+Console.ReadKey();
+
+
